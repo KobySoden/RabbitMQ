db38369080214015eaf21b671c7de27ab79a717f
KobySoden <64283612+KobySoden@users.noreply.github.com>
2022-11-18 22:24:16 -0500

    got rabbit working on my home server

--- RabbitWrap.py
+++ RabbitWrap.py
@@ -2,7 +2,7 @@ import pika
 
 def connect():
     #connect to broker
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('IP'))
     return connection.channel()
 
     
